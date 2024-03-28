8329efb7393a4a69afa3cfeeba717640d2289c92
John González Valdes <159723930+Johns1600@users.noreply.github.com>
2024-03-27 18:45:15 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Proyecto 02 programación III
+
+
+
+if __name__ == '__main__':
+
+    while True:
+        print("OP1:")
+        print("OP1:")
+        print("OP1:")
+        print("OP1:")
+        print("OP1:")
+
